09a53e8fd4492eb45f495029a29aa4df5071eb26
Mina <MinaNgh@users.noreply.github.com>
2019-01-07 17:06:23 -0800

    Update

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
+const bodyParser = require("body-parser");
+
+app.use('/',bodyParser.urlencoded({extended: false}));
+app.get("/",(req, res)=>{
+	res.sendFile(__dirname+"/view/index.html")
+})
+app.use(express.static(__dirname+"/public"));
+
+app.post("/api/upload", upload.single('file'),(req, res)=>{
+	// var file = req.query.file;
+	// res.json(req.file);
+	res.json({
+		"name": req.file.originalname,
+		"type": req.file.mimetype,
+		"size": req.file.size
+	});
+});
+
+app.listen(process.env.PORT||8080);
